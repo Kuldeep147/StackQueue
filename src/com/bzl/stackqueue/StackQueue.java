2f479af02a7521d302b77ce68a115290157b119c
Kuldeep147 <kuldeep.bargurjar@gmail.com>
2022-12-25 15:37:07 +0530

    adding uc1

--- src/com/bzl/stackqueue/StackQueue.java
+++ src/com/bzl/stackqueue/StackQueue.java
@@ -1,6 +1,20 @@
 package com.bzl.stackqueue;
+
+import java.util.LinkedList;
+
 public class StackQueue {
+    LinkedList<Integer> stack = new LinkedList<Integer>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StackQueue ob = new StackQueue();
+        ob.push(70);
+        ob.push(30);
+        ob.push(56);
+        ob.print();
+    }
+    public void push(Integer a){
+        stack.add(a);
+    }
+    public void print(){
+        System.out.println(stack);
     }
 }
